77a70c99607ddf7e246c459ce0f81a0b0fe52ba9
Danny Meyer <danny.meyer@gmail.com>
2017-11-21 11:24:50 -0500

    Basic structure, README.MD, and requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Cinema
+#### Installation
+
+pip3 install requirements.txt
+
+#####
+
+python3 monkey.py
+should play a song while sending osc values to:
+192.168.162.21
+PORT == 8000
+
+I've written a little script that detects the local ip and drops it into a variable
+I'll toss that in here at some point.
+
+
+
---
+++ monkey.py
@@ -0,0 +1,170 @@
+import random
+import time
+import pyaudio
+from clear_osc import SineOsc
+import multiprocessing as mp
+import argparse
+from pythonosc import udp_client
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="192.168.162.21",
+                    help="The ip of the OSC server")
+parser.add_argument("--port", type=int, default=8000,
+                    help="The port the OSC server is listening on")
+args = parser.parse_args()
+
+client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+
+sine_osc = SineOsc()
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paFloat32,
+                channels=1,
+                rate=44100,
+                output=1,
+                )
+
+x = 145
+freqs = [
+    x,
+    x * 9/8,
+    x * 4/3,
+    x * 5/4,
+
+    x * 3/2,
+    x * 11/8,
+    x * 3/2,
+    x * 15/18,
+
+    x * 5/1.5,
+    x * 4/6,
+    x * 9/4,
+    x * 5/4,
+
+    x * 4/1.5,
+    x * 6/2,
+    x * 15/4,
+    x * 6/2,
+
+    x * 4,
+    x * 3/2,
+    x * 3/2,
+    x * 4/3,
+
+    x * 5/4,
+    x * 3/2,
+    x * 4/3,
+    x * 5/4,
+    #
+    x * 9/8,
+    x * 3/2,
+    x * 3/2,
+    x * 4/3,
+
+    x * 11/8,
+    x * 10/4,
+    x * 10/4,
+    x * 9/4,
+
+    x * 7/4,
+    x * 3/2,
+    x * 3/2,
+    x * 5/3,
+
+    x * 11/8,
+    0,
+    x * 12/5,
+    x * 12/5,
+
+    x * 3/2,
+    x * 3/2,
+    x * 4/3,
+    x * 13/8,
+    x * 13/8,
+    x * 5/2,
+    x * 7/2,
+    x * 9/2,
+    x * 10/3,
+    ]
+
+freqs2 = [
+    x * 11/8,
+    x * 11/8,
+    0,
+    x * 12/5,
+    x * 12/5,
+
+    x * 3/2,
+    x * 3/2,
+    x * 4/3,
+    x * 13/8,
+    x * 13/8,
+    x * 5/2,
+    x * 7/2,
+    x * 9/2,
+    x * 10/3,
+    ]
+
+timesThroughBach = 2
+length = 20
+
+def func1():
+    print ('func1')
+    for i in range(timesThroughBach):
+        for freq in freqs:
+            print(freq)
+
+            client.send_message("/func1", [float(freq), float(freq), float(freq)])
+            sine_osc.play_frequencies(stream, .25, 1, 8000, 1000,
+                                      freq,
+                                      freq * 2,
+                                      freq + 2,
+                                      )
+
+
+        if i == timesThroughBach - 1:
+            time.sleep(.25)
+        time.sleep(1.9)
+
+    for i in range(length):
+        for freq in freqs2:
+            client.send_message("/func1", freq)
+            sine_osc.play_frequencies(stream, .25, 1, 8000, 1000,
+                                      freq,
+                                      freq * 2,
+                                      freq + 2,
+                                      )
+
+def func2():
+    print ('func2')
+    for i in range(timesThroughBach):
+        for freq in freqs:
+            client.send_message("/func2", [float(freq), float(freq), float(freq)])
+            sine_osc.play_frequencies(stream, .25, 1, 1000, 10000,
+                                      freq * 3/2,
+                                      freq * 3/2 * 2,
+                                      freq * 3/2,
+                                      freq / 2,
+                                      )
+
+        time.sleep(1.9)
+
+    for i in range(length):
+        for freq in freqs2:
+            client.send_message("/func2", [float(freq), float(freq), float(freq)])
+            sine_osc.play_frequencies(stream, .25, 1, 1000, 10000,
+                                      freq * 3/2,
+                                      freq * 3/2 * 2,
+                                      freq * 3/2,
+                                      freq / 2,
+                                      )
+
+
+if __name__=='__main__':
+    mp.set_start_method('spawn')
+    p1 = mp.Process(target=func1)
+    p1.start()
+    time.sleep(2)
+    p2 = mp.Process(target=func2)
+    p2.start()
