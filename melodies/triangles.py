ce63c5fd4040e56985b5ba15ed554e21043d3fe7
Danny Meyer <danny.meyer@gmail.com>
2017-11-28 09:26:31 -0500

    Adds difference tone

--- melodies/triangles.py
+++ melodies/triangles.py
@@ -20,8 +20,9 @@ def check_for_relationship(frequency1, frequency2, relationship, length):
     """Checks for given ratio."""
     # https://en.wikipedia.org/wiki/List_of_intervals_in_5-limit_just_intonation
 
-    print (frequency1, frequency2, Fraction(relationship))
     if round((frequency1/frequency2), 3) == relationship or round((frequency2/frequency1), 3) == relationship:
+        print (frequency1, frequency2, Fraction(relationship))
+
         osc.play_frequencies(
             stream,
             length,
@@ -30,7 +31,9 @@ def check_for_relationship(frequency1, frequency2, relationship, length):
             3000,
             frequency1,
             frequency2,
+            # abs(frequency1 - frequency2) #uncomment to hear difference tone
             # frequency1 + frequency2 #uncomment to hear summation tone
+
         )
 
 def generate_test_array():
@@ -47,12 +50,12 @@ if __name__ == '__main__':
 
     for frequency1, frequency2 in test_freqs:
         # check_for_relationship(frequency1, frequency2, (2/1), 3) # Octave
-        check_for_relationship(frequency1, frequency2, (3/2), 3) # Perfect Fifth
+        # check_for_relationship(frequency1, frequency2, (3/2), 3) # Perfect Fifth
         # check_for_relationship(frequency1, frequency2, (4/3), 4) # Perfect Fourth
-        # check_for_relationship(frequency1, frequency2, (9/8), 3) # Major Second
+        check_for_relationship(frequency1, frequency2, (9/8), 3) # Major Second
         #
-        check_for_relationship(frequency1, frequency2, (5/4), 3) # Major Third
-        # check_for_relationship(frequency1, frequency2, (6/5), 3) # Minor Third
-        # check_for_relationship(frequency1, frequency2, (15/8), 3) # Major Seventh
+        # check_for_relationship(frequency1, frequency2, (5/4), 3) # Major Third
+        check_for_relationship(frequency1, frequency2, (6/5), 3) # Minor Third
+        check_for_relationship(frequency1, frequency2, (15/8), 3) # Major Seventh
         #
         # check_for_relationship(frequency1, frequency2, (11/8), 3) # 11/8 'in-tune' tritone
